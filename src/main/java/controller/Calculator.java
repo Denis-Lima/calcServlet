0d34de983594c5ccfde3553e8e9741fc538ca471
Denis Lima <denislima99@gmail.com>
2021-08-14 13:00:22 -0300

    Configurando página inicial

--- src/main/java/controller/Calculator.java
+++ src/main/java/controller/Calculator.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = {"/"})
+@WebServlet(urlPatterns = {"/teste"})
 public class Calculator extends HttpServlet {
 
     @Override
